2c2fadf21eb2e5b79dd87337c94e4d14a4f407f1
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2023-05-23 07:26:48 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,80 +4,60 @@ import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem';
 import { LoadMoreBtn } from './Button';
-// import { FetchRequest } from './fetchRequest';
+import { Loader } from './Loader';
 
 export class App extends Component {
   state = {
     images: [],
+    loading: false,
   };
 
   page = 1;
 
-  componentDidMount() {}
-
   fetchRequest = () => {
     const input = document.querySelector('input');
     const inputValue = input.value;
-    fetch(
-      `https://pixabay.com/api/?key=34824260-e95f578da3e246504fd89f51b&q=${inputValue}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`
-    )
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.hits);
-        this.setState({ images: [...this.state.images, ...data.hits] });
-        console.log(this.state);
-      })
-      .catch(err => console.log(err));
+
+    this.setState({ loading: true, images: [] });
+    setTimeout(() => {
+      fetch(
+        `https://pixabay.com/api/?key=34824260-e95f578da3e246504fd89f51b&q=${inputValue}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`
+      )
+        .then(response => response.json())
+        .then(data => {
+          // console.log(data.hits);
+          this.setState({ images: [...this.state.images, ...data.hits] });
+          // console.log(this.state);
+        })
+        .catch(err => console.log(err))
+        .finally(() => {
+          this.setState({ loading: false });
+        });
+    }, 2000);
   };
 
   handleLoadMoreRequest = () => {
-    const input = document.querySelector('input');
-    const inputValue = input.value;
+    // const input = document.querySelector('input');
+    // const inputValue = input.value;
     this.page += 1;
 
     this.fetchRequest();
-    // fetch(
-    //   `https://pixabay.com/api/?key=34824260-e95f578da3e246504fd89f51b&q=${inputValue}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`
-    // )
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log(data.hits);
-    //     this.setState({ images: [...this.state.images, ...data.hits] });
-
-    //     console.log(this.state);
-    //   })
-    //   .catch(err => console.log(err));
   };
 
   handleRequest = e => {
     e.preventDefault();
     this.fetchRequest();
-    // const input = document.querySelector('input');
-    // const inputValue = input.value;
-
-    // console.log(inputValue);
-
-    //   fetch(
-    //     `https://pixabay.com/api/?key=34824260-e95f578da3e246504fd89f51b&q=${inputValue}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`
-    //   )
-    //     .then(response => response.json())
-    //     .then(data => {
-    //       console.log(data.hits);
-    //       this.setState({ images: [...this.state.images, ...data.hits] });
-
-    //       console.log(this.state);
-    //     })
-    //     .catch(err => console.log(err));
   };
 
   render() {
-    console.log(this.state);
+    const { loading, images } = this.state;
     return (
       <>
         <Searchbar onSubmit={this.handleRequest} />
-        {this.state.images.length > 0 && (
+        {loading && <Loader />}
+        {images.length > 0 && (
           <ImageGallery>
-            {this.state.images.map(image => {
+            {images.map(image => {
               return (
                 <ImageGalleryItem
                   key={image.id}
@@ -87,7 +67,7 @@ export class App extends Component {
             })}
           </ImageGallery>
         )}
-        {this.state.images.length > 0 && (
+        {images.length > 0 && (
           <LoadMoreBtn onClick={this.handleLoadMoreRequest} />
         )}
       </>
