9acc663841166ea1157aab7788fc464531451967
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2023-05-25 07:09:48 +0200

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Component } from 'react';
-import { Searchbar } from './Searchbar';
-import { ImageGallery } from './ImageGallery';
-import { ImageGalleryItem } from './ImageGalleryItem';
-import { LoadMoreBtn } from './Button';
-import { Loader } from './Loader';
-import Modal from './Modal';
+import Searchbar from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { LoadMoreBtn } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import Modal from './Modal/Modal';
 
 export class App extends Component {
   state = {
@@ -14,25 +14,48 @@ export class App extends Component {
     // status: 'idle',
     showModal: false,
     selectedImage: null,
+    searchQuiry: '',
   };
 
   page = 1;
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate befor');
+    console.log(prevState);
+    if (prevState.searchQuiry !== this.state.searchQuiry) {
+      this.fetchRequest();
+      // console.log('componentDidUpdate in if');
+      // console.log('prevState.value:', prevState.searchQuiry);
+      // console.log(this.state.searchQuiry);
+    }
+  }
 
   toggleModal = image => {
     this.setState(prevState => ({ selectedImage: image }));
   };
 
   fetchRequest = () => {
-    const input = document.querySelector('input');
-    const inputValue = input.value;
-
     this.setState({ loading: true });
+
     setTimeout(() => {
       fetch(
-        `https://pixabay.com/api/?key=34824260-e95f578da3e246504fd89f51b&q=${inputValue}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`
+        `https://pixabay.com/api/?key=34824260-e95f578da3e246504fd89f51b&q=${this.state.searchQuiry}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}&editors_choice=id,webformatURL,largeImageURL,tags`
       )
         .then(response => response.json())
         .then(data => {
+          console.log(data);
+          if (data.total === 0) {
+            alert('No results found!');
+            return;
+          }
+
+          // if (data.total <= 12) {
+          //   const btn = document.querySelector('#load-more');
+          //   btn.addAttribute('disabled');
+
+          //   console.log('data <12');
+          // }
+
           this.setState({
             images: [...this.state.images, ...data.hits],
             loading: false,
@@ -52,10 +75,12 @@ export class App extends Component {
     this.fetchRequest();
   };
 
-  handleRequest = e => {
-    e.preventDefault();
-    this.setState({ images: [] });
-    this.fetchRequest();
+  handleRequest = value => {
+    // e.preventDefault();
+    this.page = 1;
+
+    this.setState({ searchQuiry: value, images: [] });
+    console.log(value);
   };
 
   render() {
@@ -108,7 +133,6 @@ export class App extends Component {
             <img src={selectedImage.largeImageURL} alt={selectedImage.name} />
           </Modal>
         )}
-
         {loading && <Loader />}
         {images.length > 0 && (
           <ImageGallery>
@@ -125,9 +149,13 @@ export class App extends Component {
             })}
           </ImageGallery>
         )}
+
         {images.length > 0 && (
           <LoadMoreBtn onClick={this.handleLoadMoreRequest} />
         )}
+        {/* {images.length <= 12 ? null : (
+          <LoadMoreBtn disabled onClick={this.handleLoadMoreRequest} />
+        )} */}
       </>
     );
   }
